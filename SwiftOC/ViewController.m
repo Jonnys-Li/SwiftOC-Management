a2a25ea2ed191d6ee39c84939a48080fbb629e89
Jonnys-Li <2862198176@qq.com>
2026-05-12 18:10:07 +0800

    feat: integrate UserManager into ViewController - demo CRUD operations

--- SwiftOC/ViewController.m
+++ SwiftOC/ViewController.m
@@ -1,5 +1,6 @@
 #import "ViewController.h"
 #import "SwiftOC-Swift.h"
+#import "UserManager.h"
 
 @interface ViewController ()
 
@@ -10,11 +11,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // --- Swift & OC interop demo ---
     SwiftClass *swift = [[SwiftClass alloc] initWithName:@"OC"];
     NSLog(@"%@", [swift greeting]);
-
     NSLog(@"%@", [SwiftClass callOC]);
 
+    // --- UserManager demo ---
+    UserManager *mgr = [UserManager sharedInstance];
+    [mgr addUser:[[UserModel alloc] initWithUserId:1 name:@"Alice" role:@"Admin"]];
+    [mgr addUser:[[UserModel alloc] initWithUserId:2 name:@"Bob"   role:@"Editor"]];
+    [mgr addUser:[[UserModel alloc] initWithUserId:3 name:@"Charlie" role:@"Viewer"]];
+
+    NSLog(@"Total users: %ld", (long)[mgr userCount]);
+    for (UserModel *u in [mgr listUsers]) {
+        NSLog(@"%@", u);
+    }
+
+    UserModel *found = [mgr findUserById:2];
+    NSLog(@"Found user 2: %@", found);
+
+    // Warning: removeUserById: in v1.0.0 has NO bounds check
+    // Passing userId that doesn't exist (e.g. 99) will crash
+    // [mgr removeUserById:99];  // ← index out of bounds crash
+    
+    [mgr removeUserById:0];
+    NSLog(@"After removal, count: %ld", (long)[mgr userCount]);
+
+    // --- UI label ---
     UILabel *label = [[UILabel alloc] init];
     label.text = [swift greeting];
     label.textAlignment = NSTextAlignmentCenter;
